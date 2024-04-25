a8cf23fff94208362a5c7d4cf55d47e66ff28894
Mohammad Bashtani <mohammadbashtani85@gmail.com>
2024-04-25 19:45:14 +0330

    Style Coins

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,6 +1,9 @@
 import { MagnifyingGlass } from "react-loader-spinner";
+
 import chartUp from "../../assets/chart-up.svg";
 import chartDown from "../../assets/chart-down.svg";
+
+import styles from "./TableCoin.module.css";
 
 function TableCoin({ coins, isLoading }) {
   console.log(coins);
@@ -9,7 +12,7 @@ function TableCoin({ coins, isLoading }) {
       {isLoading ? (
         <MagnifyingGlass />
       ) : (
-        <table>
+        <table className={styles.table}>
           <thead>
             <tr>
               <th>Coin</th>
@@ -45,14 +48,14 @@ const TableRow = ({
   return (
     <tr>
       <td>
-        <div>
+        <div className={styles.symbol}>
           <img src={image} alt={name} />
           <span>{symbol.toUpperCase()}</span>
         </div>
       </td>
       <td>{name}</td>
-      <td>{current_price.toLocaleString()}</td>
-      <td>{price_change.toFixed(2)}%</td>
+      <td>${current_price.toLocaleString()}</td>
+      <td className={price_change > 0? styles.success: styles.error}>{price_change.toFixed(2)}%</td>
       <td>{total_volume.toLocaleString()}</td>
       <td>
         <img src={price_change > 0 ? chartUp : chartDown} alt={name} />
